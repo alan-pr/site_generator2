678b2b427adf2eff2935a21f74ee9495220cef47
Alan <alan.pr88@gmail.com>
2025-03-24 16:53:39 -0500

    parse_link_and_image_texts function added to parser.py

--- public/src/parser.py
+++ public/src/parser.py
@@ -1,4 +1,5 @@
 from enum import Enum
+import re
 
 
 class TextType(Enum):
@@ -10,6 +11,14 @@ class TextType(Enum):
     IMAGE = "image"
 
 
+# Constants
+TEXT_FORMATS = [TextType.TEXT, TextType.BOLD, TextType.ITALIC, TextType.CODE]
+TEXT_LINKS = [TextType.LINK, TextType.IMAGE]
+LINK_REGEX = r"(?<!\!)\[(.*?)\]\((.*?)\)"
+IMAGE_REGEX = r"!\[(.*?)\]\((.*?)\)"
+
+
+# Static Functions
 def convert_to_html(text):
     return parse(text)
 
@@ -20,8 +29,28 @@ def parse(text):
         text_type = get_foremost_symbol(html_text)
         if text_type is None:
             break
-        html_text = parse_delimiter(html_text, text_type)
+        if text_type == TextType.TEXT and text_type not in TEXT_FORMATS[1:]:
+            html_text = parse_link_and_image_texts(html_text)
+        else:
+            html_text = parse_delimiter(html_text, text_type)
     return html_text
+
+
+def parse_delimiter(text, text_type):
+    delimiter = get_text_type_symbol(text_type)
+    paragraphs = text.split("\n\n")
+    html_paragraphs = []
+
+    for paragraph in paragraphs:
+        if text.count(delimiter) > 1:
+            if text.count(delimiter) % 2 == 0:
+                html_text = split_text_delimiter(paragraph, delimiter, text_type)
+            else:
+                html_text = split_text_outer_delimiter(paragraph, delimiter, text_type)
+        else:
+            html_text = text
+        html_paragraphs.append(html_text.replace("\n", ' '))
+    return ''.join(html_paragraphs)
 
 
 def split_text_delimiter(text, delimiter, text_type):
@@ -38,19 +67,6 @@ def split_text_delimiter(text, delimiter, text_type):
     return f"{before}<{tag}>{middle}</{tag}>{after}"
 
 
-def parse_delimiter(text, text_type):
-    delimiter = get_text_type_symbol(text_type)
-
-    if text.count(delimiter) > 1:
-        if text.count(delimiter) % 2 == 0:
-            html_text = split_text_delimiter(text, delimiter, text_type)
-        else:
-            html_text = split_text_outer_delimiter(text, delimiter, text_type)
-    else:
-        html_text = text
-    return html_text
-
-
 def split_text_outer_delimiter(text, delimiter, text_type):
     start = text.find(delimiter)
     end = text.rfind(delimiter)
@@ -65,6 +81,22 @@ def split_text_outer_delimiter(text, delimiter, text_type):
     return f"{before}<{tag}>{middle}</{tag}>{after}"
 
 
+def parse_link_and_image_texts(text):
+    html_text = text
+    if is_markdown_link_present(text):
+        links = extract_markdown_links(text)
+        for link in links:
+            formatted_link = f"<a href=\"{link[1]}\">{link[0]}</a>"
+            html_text = re.sub(LINK_REGEX, formatted_link, html_text, count=1)
+    return html_text
+
+
+def add_paragraph_tags(text):
+    if text[:3] == "<p>" and text[-4:]:
+        return text
+    return "<p>" + text + "</p>"
+
+
 def get_foremost_symbol(text):
     foremost_text_type = None
     foremost_index = float("inf")
@@ -74,16 +106,20 @@ def get_foremost_symbol(text):
             continue
         if text.count(get_text_type_symbol(text_type)) < 2:
             continue
-        symbol = get_text_type_symbol(text_type)
-        current_index = text.find(symbol)
-        if 0 <= current_index < foremost_index:
-            foremost_index = current_index
-            foremost_text_type = text_type
-
+        if text_type in TEXT_FORMATS[1:]:
+            symbol = get_text_type_symbol(text_type)
+            current_index = text.find(symbol)
+            if 0 <= current_index < foremost_index:
+                foremost_index = current_index
+                foremost_text_type = text_type
     if foremost_text_type is None:
         return None
-
     return foremost_text_type
+
+
+def get_non_format_text(text_type):
+    pass
+
 
 
 def get_text_type_symbol(text_type):
@@ -107,6 +143,7 @@ def get_tag(text_type):
         case TextType.CODE:
             return "code"
 
+
 def is_symbol_valid(symbol):
     match symbol:
         case TextType.BOLD | TextType.ITALIC | TextType.CODE:
@@ -119,10 +156,20 @@ def is_symbol_present(text, symbol):
     return is_symbol_valid(symbol) and text.count(symbol) > 1
 
 
-text = """**This is a bold text with an _italic text_ inside** and also this
-This an italic text_**
-This is a `_code text_`
-This is another **bold text**
-    """
+def extract_markdown_images(text):
+    return re.findall(IMAGE_REGEX, text)
+
+
+def extract_markdown_links(text):
+    return re.findall(LINK_REGEX, text)
+
+
+def is_markdown_link_present(text):
+    return re.search(LINK_REGEX, text) is not None
+
+
+
+# text = "This is a plain text. [Google](www.google.com) and [Test](www.test.com)"
+text = "This is a bold text."
 print()
-print(convert_to_html(text))
+print(parse(text))
